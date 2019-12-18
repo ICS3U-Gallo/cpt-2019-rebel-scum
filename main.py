2af8fb70d0c940110e00438f0523e418623d74bc
ethan_alvares <47458407+ethanalvares@users.noreply.github.com>
2019-12-18 09:30:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import settings
 
 from menu import MenuView
 from chapter_1 import Chapter1View
+from chapter3 import Chapter3View
 
 
 class Director(arcade.Window):
@@ -12,7 +13,8 @@ class Director(arcade.Window):
         self.view_index = 0
         self.views = [
             MenuView,
-            Chapter1View
+            Chapter1View,
+            Chapter3View
         ]
         self.next_view()
 
