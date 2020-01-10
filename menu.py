f2ece1985f957e79794f4244b3a845b9fbc14b93
ethan_alvares <47458407+ethanalvares@users.noreply.github.com>
2020-01-10 09:45:12 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,16 +1,39 @@
 import arcade
-
 import settings
+
+starcount = 50
+self.star_sprite_list = None
 
 
 class MenuView(arcade.View):
     def on_show(self):
-        arcade.set_background_color(arcade.color.WHITE)
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def setup(self):
+        self.star_sprite_list = arcade.SpriteList()
+
+        for i in range(starcount):
+            star = arcade.draw_circle_filled(star.center_x, star.center_y, 10,
+                                             arcade.color.WHITE)
+
+            star.centre_x = random.randrange(settings.WIDTH)
+            star.centre_y = random.randrange(settings.HEIGHT)
+
+            self.star_sprite_list.append(star)
+
+    def update(self):
+        star.centre_x += 1
+
+        if star.centre_x > settings.WIDTH:
+            star.centre_x = 0
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Menu", settings.WIDTH/2, settings.HEIGHT/2,
-                         arcade.color.BLACK, font_size=30, anchor_x="center")
+        self.star_sprite_list.draw()
+
+        arcade.draw_texture_rectangle(settings.WIDTH/2, settings.HEIGHT/2,
+                                      settings.WIDTH-100, settings.HEIGHT-200,
+                                      alvares/starwars/Star_Wars_Logo.png)
 
     def on_key_press(self, key, modifiers):
         self.director.next_view()
